c92ef8d37731c465b2cd7e81fe2a7f8b388ff351
Brayden Aimar <baimar97@hotmail.com>
2017-03-17 13:22:01 -0700

    Setup program files

---
+++ js/lib/require-config.js
@@ -0,0 +1,12 @@
+
+requirejs.config({
+	baseUrl: 'js',
+	paths: {
+		jquery: 'lib/jquery',
+		amplify: 'lib/amplify.core',
+		gui: 'lib/gui',
+		bootstrap: 'lib/bootstrap',
+		mousetrap: 'lib/mousetrap.min'
+	}
+    // nodeRequire: require
+});
